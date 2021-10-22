fc7dc6277b78153e0aa4f978d872f07e39421160
Viacheslav_Bondariev <rdy2qa@gmail.com>
2021-10-22 14:01:09 +0300

    HW14

---
+++ hillelPro/lesson14/homework14/app.js
@@ -0,0 +1,16 @@
+
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".header-nav")
+
+burger.addEventListener('click', () => {
+
+    nav.classList.toggle("flex");
+    if (nav.classList.contains("flex")) {
+        nav.style.left = "0%"
+        burger.style.backgroundImage = "url(\"img/cross.png\")"
+    }else {
+        burger.style.backgroundImage = "url(\"img/burger.png\")"
+        nav.style.left = "-100%"
+    }
+})
